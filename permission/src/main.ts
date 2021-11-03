2a0c3f5d26d0dab29bd230eeed45dc532adacfca
Giovanni Costa <cgiovanni1996@gmail.com>
2021-11-03 16:06:44 +0100

    :sparkles: permission / basic configuration

--- permission/src/main.ts
+++ permission/src/main.ts
@@ -1,8 +1,25 @@
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { PermissionModule } from './permission.module';
+import { ConfigService, RabbitConf } from './config/config.service';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const rabbit = new ConfigService().get<RabbitConf>('rabbit');
+
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    PermissionModule,
+    {
+      transport: Transport.RMQ,
+      options: {
+        urls: [rabbit.host],
+        queue: rabbit.queue,
+        queueOptions: {
+          durable: true,
+        },
+      },
+    },
+  );
+
+  await app.listen();
 }
 bootstrap();
